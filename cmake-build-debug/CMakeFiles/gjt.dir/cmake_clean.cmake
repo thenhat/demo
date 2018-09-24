578dafdfe2e7320462085c76decf700c8d87fb86
thenhat <thenhat@vics-MacBook-Pro.local>
2018-09-24 19:11:48 +0700

    suwa

---
+++ cmake-build-debug/CMakeFiles/gjt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gjt.dir/main.c.o"
+  "gjt.pdb"
+  "gjt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gjt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("nhatntth!\n");
+    return 0;
+}
